67753d9905390d2f41c127d9e0ac2141ec798a55
ym4shida <github@myosotis.dev>
2022-06-03 17:12:16 +0900

    initial commit  - ready to run!

---
+++ README.md
@@ -0,0 +1,6 @@
+# uninstall Docker Desktop for mac
+can use for uninstall Docker Desktop for mac.
+
+## how to use
+- quit Docker Desktop and docker-deamon.
+- type `sh uninstall-docker.sh` end return!
---
+++ uninstall-docker.sh
@@ -0,0 +1,19 @@
+sudo rm -vRf /Applications/Docker.app
+sudo rm -vf /usr/local/bin/docker
+sudo rm -vf /usr/local/bin/docker-machine
+sudo rm -vf /usr/local/bin/docker-compose
+sudo rm -vf /usr/local/bin/docker-credential-desktop
+sudo rm -vf /usr/local/bin/docker-credential-ecr-login
+sudo rm -vf /usr/local/bin/docker-credential-osxkeychain
+sudo rm -vRf ~/.docker
+sudo rm -vRf ~/Library/Containers/com.docker.docker
+sudo rm -vRf ~/Library/Application\ Support/Docker\ Desktop
+sudo rm -vRf ~/Library/Group\ Containers/group.com.docker
+sudo rm -vf ~/Library/HTTPStorages/com.docker.docker.binarycookies
+sudo rm -vf /Library/PrivilegedHelperTools/com.docker.vmnetd
+sudo rm -vf /Library/LaunchDaemons/com.docker.vmnetd.plist
+sudo rm -vRf ~/Library/Logs/Docker\ Desktop
+sudo rm -vRf /usr/local/lib/docker
+sudo rm -vf ~/Library/Preferences/com.docker.docker.plist
+sudo rm -vRf ~/Library/Saved\ Application\ State/com.electron.docker-frontend.savedState
+sudo rm -vf ~/Library/Preferences/com.electron.docker-frontend.plist
